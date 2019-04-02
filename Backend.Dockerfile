82a5107ade868bf989ee860fe3d3f29dcb263830
cjkeilig <cjkeilig@gmail.com>
2019-04-01 21:10:16 -0500

    Test changes from heroku-integration in jFranco's branch

---
+++ Backend.Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3
+
+# Drop log meesages directly back to the console
+ENV PYTHONBUFFERED 1
+
+RUN mkdir /app
+WORKDIR /app
+
+# Install dependencies
+COPY requirements.txt /app/
+RUN pip install -r requirements.txt
--- stem/settings.py
+++ stem/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import datetime
 import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -101,7 +102,6 @@ else:
             'PORT': '5432',
         }
     }
-
 
 
 # Password validation
